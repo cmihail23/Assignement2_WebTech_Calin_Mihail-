5f55dabe1fdb3c62700bf0a0c0bf69061138cbc2
Mihail <mihailcalin.2002@yahoo.com>
2022-11-18 21:19:13 +0200

    rezolvare

---
+++ main/app.js
@@ -0,0 +1,35 @@
+function addTokens(input, tokens) {
+    if (typeof input != "string") {
+        throw new Error('Invalid input');
+    }
+    if (input.length < 6) {
+        throw new Error('Input should have at least 6 characters')
+    }
+
+    if (tokens==null) {
+        throw new Error('Invalid array format')
+    }
+    if(typeof tokens[0]['tokenName'] != "string"){
+        throw new Error('Invalid array format')
+    }
+
+    if (input.includes('...')) {
+        let i=0;
+        while (input.includes('...')) {
+            let val = tokens[i];
+            let valoare = val['tokenName'];
+            let nuEBunTestul = '${'+valoare+'}';
+            input = input.replace("...",nuEBunTestul)
+            i++;
+        }
+    }
+
+    return input;
+}
+
+const app = {
+    addTokens: addTokens
+}
+addTokens('Subsemnatul ...', [{tokenName: 'subsemnatul'}])
+
+module.exports = app;
